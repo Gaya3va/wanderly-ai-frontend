7b54cc34e562bb79ff69907a83bb31ca2c09bf53
Gaya3va <gayatreecode@gmail.com>
2026-03-02 23:51:10 +0530

    final commit

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,7 @@ import axios from "axios";
 */
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wanderley-ai-backend.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
